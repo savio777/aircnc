144d61db72944cd33afe144f14cedad03020e324
Sávio <saviopf22@gmail.com>
2019-10-02 17:41:30 -0300

    instalação de nodemon para facilitar o desenvolvimento e teste de parametros em rotas padrões em uma api rest

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,9 +4,32 @@ const app = express()
 
 const port = 7777
 
-app.get('/', (req, res) => {
-    res.send('hello world express:)')
-    console.log('teste')
+// servidor aceitar requisições json
+app.use(express.json())
+
+// LINGUAGEM DA WEB
+// GET, POST, PUT, DELETE
+
+// teste query ex: '?name=blabla'
+app.get('/test', (req, res) => {
+    res.json({ name: req.query.name })
+    console.log('oi:)')
 })
 
-app.listen(port)
+// teste body e params 
+app.put('/test/:id', (req, res) => {
+    res.json({
+        id: req.params.id,
+        name: req.body.name
+    })
+    console.log('oi:)')
+})
+
+app.post('/test', (req, res) => {
+    res.json({ name: req.body.name })
+    console.log('oi:)')
+})
+
+app.listen(port, () => {
+    console.log(`server is running in localhost:${port}`)
+})
