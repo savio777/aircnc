71235b16c7477aa0c8b287c8527a85d1ccfa2a8c
Sávio <saviopf22@gmail.com>
2019-10-02 22:17:32 -0300

    model do usuario e controller do usuario

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,34 +1,23 @@
 const express = require('express')
 
+const mongoose = require('mongoose')
+
 const app = express()
+
+const routes = require('./routes')
 
 const port = 7777
 
-// servidor aceitar requisições json
+mongoose.connect('mongodb://localhost:27017/omnistack9',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
+
 app.use(express.json())
 
-// LINGUAGEM DA WEB
-// GET, POST, PUT, DELETE
-
-// teste query ex: '?name=blabla'
-app.get('/test', (req, res) => {
-    res.json({ name: req.query.name })
-    console.log('oi:)')
-})
-
-// teste body e params 
-app.put('/test/:id', (req, res) => {
-    res.json({
-        id: req.params.id,
-        name: req.body.name
-    })
-    console.log('oi:)')
-})
-
-app.post('/test', (req, res) => {
-    res.json({ name: req.body.name })
-    console.log('oi:)')
-})
+app.use(routes)
 
 app.listen(port, () => {
     console.log(`server is running in localhost:${port}`)
