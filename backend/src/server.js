5ac48de286875810936108b8f6ae6fb2efaf68d8
Sávio <saviopf22@gmail.com>
2019-10-11 17:32:56 -0300

    adicionado o cors ao servidor para que possa ser acessado remotamente

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const mongoose = require('mongoose')
 
+const cors = require('cors')
+
 const app = express()
 
 const routes = require('./routes')
@@ -15,8 +17,11 @@ mongoose.connect('mongodb://localhost:27017/omnistack9',
     }
 )
 
-app.use(express.json())
+// com o cors é possível definir urls que vão poder acessar o servidor
+//app.use(cors({ origin: 'http://localhost:3000' }))
 
+app.use(cors())
+app.use(express.json())
 app.use(routes)
 
 app.listen(port, () => {
