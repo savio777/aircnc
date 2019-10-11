9df22b443282e441790bc81350acae78f80fb8da
Sávio <saviopf22@gmail.com>
2019-10-11 17:00:46 -0300

    tela de login finalizada e axios instalado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
+import './App.css'
+
+import logo from './assets/logo.svg'
+
 const App = () => {
-  return(
+  return (
     <div>
-      <a>oi</a>
+      <div className="container">
+        <img src={logo} alt="Logo AirCnC" />
+        <div className="content">
+          <p>
+            Ofereça <strong>spots</strong> para programadores
+            e encontre <strong>talentos</strong> para sua empresa
+          </p>
+          <form method="POST" action="">
+            <label htmlFor="email">EMAIL *</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Seu melhor Email"
+            />
+            <button className="btn" type="submit">Entrar</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
