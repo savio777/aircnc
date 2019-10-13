e89aa972d2d280ea7fb2e890ed2741ef53c07ec6
Sávio <saviopf22@gmail.com>
2019-10-13 01:25:12 -0300

    tela do dashboard finalizada

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { Link } from 'react-router-dom'
 
 import api from '../../services/api'
 
+import './styles.css'
+
 function Dashboard() {
 
-  // 1 HORA DE VIDEO
-  //  const [spots, setSpots] = useState(null)
+  const [spots, setSpots] = useState([])
 
   useEffect(() => {
     async function loadSpots() {
@@ -16,18 +19,33 @@ function Dashboard() {
         }
       })
 
-      // teste
-      console.log(response.data)
+      setSpots([...response.data])
     }
 
     loadSpots()
 
   }, [])
 
+  // VIDEO INTERFACE WEB 1:10
+
   return (
-    <div >
-      <h1>iaekkk</h1>
-    </div>
+    <>
+      <ul className="spot-list">
+        {(spots) && (spots.map((value) => (
+          <li key={value._id}>
+            <header style={{ backgroundImage: `url(${value.thumbnail_url})` }} />
+            <strong>{value.company}</strong>
+            <span>{(value.price) ? `R$ ${value.price}/dia` : 'FREE'}</span>
+          </li>
+        )))}
+      </ul>
+
+      <Link to="/newspot">
+        <button className="btn">
+          Cadastrar Novo Spot
+        </button>
+      </Link>
+    </>
   )
 }
 
